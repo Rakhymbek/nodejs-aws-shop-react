7939e63c67fae225bba4ae680aa21c86406e618a
rakhymbek <sagrakha@gmail.com>
2023-06-20 01:40:14 +0600

    refactor: change apiPaths for import/

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5xyn1r67jf.execute-api.us-east-1.amazonaws.com",
   bff: "https://wvag8l0p53.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
